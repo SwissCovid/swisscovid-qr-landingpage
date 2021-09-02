c5cfde9c04fcea0bd79c8d1339a51f0c60fb9ea0
Zeno Koller <koller@ubique.ch>
2021-09-02 11:13:07 +0200

    Remove AppStore redirect

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,12 +5,3 @@ var ready = (fn) => {
         document.addEventListener("DOMContentLoaded", fn);
     }
 };
-
-ready(() => {
-    if (navigator.userAgent.toLowerCase().indexOf("android") > -1) {
-        window.location.href = window.currentLanguage.playStoreLink;
-    }
-    if (navigator.userAgent.toLowerCase().indexOf("iphone") > -1) {
-        window.location.href = window.currentLanguage.appStoreLink;
-    }
-});
